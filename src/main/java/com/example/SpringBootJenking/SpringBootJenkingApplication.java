df1fcaf4bb9857391e5525fdee4b575fe3623d9c
shubhamithub <149042875+shubhamithub@users.noreply.github.com>
2024-09-07 17:01:10 +0530

    Add files via upload
    First changes

---
+++ src/main/java/com/example/SpringBootJenking/SpringBootJenkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJenking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootJenking/SpringBootJenkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJenking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
